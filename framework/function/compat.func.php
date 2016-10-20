68288ea858bf4509fae611636b5253452fe9b881
RenChao <donknap@qq.com>
2016-10-20 16:52:49 +0800

    增加兼容hex2bin函数

--- framework/function/compat.func.php
+++ framework/function/compat.func.php
@@ -80,3 +80,13 @@ if (!defined('JSON_UNESCAPED_UNICODE')) {
 	define('JSON_UNESCAPED_UNICODE', 256);
 }
 
+if (!function_exists('hex2bin')) {
+	function hex2bin($str) {
+		$sbin = '';
+		$len = strlen($str);
+		for ($i = 0; $i < $len; $i += 2) {
+			$sbin .= pack("H*", substr($str, $i, 2));
+		}
+		return $sbin;
+	}
+}
