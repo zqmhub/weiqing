86e1033211d8a842fa5bc39fc3363241dbbc5e76
gordensong <94581976@qq.com>
2016-10-25 09:57:11 +0800

    广告联盟及模块安装 bug

--- framework/version.inc.php
+++ framework/version.inc.php
@@ -5,6 +5,6 @@
  */
 defined('IN_IA') or exit('Access Denied');
 
-define('IMS_FAMILY', 'x');
+define('IMS_FAMILY', 'v');
 define('IMS_VERSION', '0.6');
 define('IMS_RELEASE_DATE', '20140807141111');
