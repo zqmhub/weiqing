7bc013bc9762a96739c1d88401bf0fa67a399c29
RenChao <donknap@qq.com>
2016-11-03 17:08:45 +0800

    优化粉丝搜索，增加模糊，精确查询

--- web/source/utility/sync.ctrl.php
+++ web/source/utility/sync.ctrl.php
@@ -13,10 +13,9 @@ if(!empty($_W['uniacid'])) {
 	if($sync != 1) {
 		exit();
 	}
-
 	// 这里仅取了10条数据，进行初始化
 	if($_W['account']['type'] == 1 && $_W['account']['level'] >= 3) {
-		$data = pdo_fetchall('SELECT fanid, openid, acid, uid, uniacid FROM ' . tablename('mc_mapping_fans') . " WHERE uniacid = :uniacid AND acid = :acid AND follow = 1 ORDER BY updatetime ASC, fanid DESC LIMIT 10", array(':uniacid' => $_W['uniacid'], ':acid' => $_W['acid']));
+		$data = pdo_fetchall('SELECT fanid, openid, acid, uid, uniacid FROM ' . tablename('mc_mapping_fans') . " WHERE uniacid = :uniacid AND acid = :acid AND follow = '1' ORDER BY fanid DESC LIMIT 10", array(':uniacid' => $_W['uniacid'], ':acid' => $_W['acid']));
 		if(!empty($data)) {
 			foreach($data as $row) {
 				mc_init_fans_info($row);
